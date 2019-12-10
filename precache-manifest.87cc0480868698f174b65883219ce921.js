3430b779a7ea39704bf2844d34fa18c598dcd43b
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2019-12-10 16:18:02 +0100

    Updates

---
+++ precache-manifest.87cc0480868698f174b65883219ce921.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3065af143c109aa9d9ea3e3fa68486f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "efee74176a8fd34636da",
+    "url": "/robofriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "0b301000e037589bc41c",
+    "url": "/robofriends/static/css/main.c9f786ac.chunk.css"
+  },
+  {
+    "revision": "efee74176a8fd34636da",
+    "url": "/robofriends/static/js/2.6d59ea6c.chunk.js"
+  },
+  {
+    "revision": "0b301000e037589bc41c",
+    "url": "/robofriends/static/js/main.6552a18c.chunk.js"
+  },
+  {
+    "revision": "cbed4829f530da065628",
+    "url": "/robofriends/static/js/runtime-main.07d9c460.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
